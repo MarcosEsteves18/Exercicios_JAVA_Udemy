22806d41dafe8354246d18b3001cea99433c275f
MarcosEsteves18 <vi_ni_esteves18@hotmail.com>
2025-02-04 15:56:00 -0300

    Aula 25

--- aula_25_processamento_de_dados/src/Main.java
+++ aula_25_processamento_de_dados/src/Main.java
@@ -4,12 +4,30 @@ public class Main {
 	public static void main(String[] args) {
 		int x;
 		double y;
-		
+
 		x = 5;
 		y = 2 * x;
-		
+
 		System.out.println(x);
-		System.out.println(y);	
+		System.out.println(y);
+
+		System.out.println("=====================================");
+
+		double b, B, h, area;
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		area = (b + B) / 2.0 * h;
+		System.out.println(area);
+		
+		
+		System.out.println("=====================================");
+		int a, c;
+		double resultado;
+		a = 5;
+		c = 2;
+		resultado = (double ) a / c;
+		System.out.println(resultado);
 
 	}
 
